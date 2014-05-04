2881a359b5c1d2a0352af136860f8abc38946a34
Denis Parchenko <denis.parchenko@gmail.com>
2014-05-04 21:03:01 +0300

    Perform replacements in all files, including reved ones
    Also add .jshintrc file. Fix jshint issues

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
-var crypto = require('crypto');
+
 var path = require('path');
 var gutil = require('gulp-util');
 var through = require('through2');
+var replaceInExtensions = ['.js', '.css', '.html'];
 
 function relPath(base, filePath) {
   var newPath = filePath.replace(base, '');
@@ -13,11 +14,11 @@ function relPath(base, filePath) {
   }
 }
 
-var plugin = function () {
+var plugin = function() {
   var renames = {};
   var cache = [];
 
-  return through.obj(function (file, enc, cb) {
+  return through.obj(function(file, enc, cb) {
     if (file.isNull()) {
       this.push(file);
       return cb();
@@ -28,28 +29,33 @@ var plugin = function () {
       return cb();
     }
 
+    // Collect renames from reved files.
     if (file.revOrigPath) {
       renames[relPath(file.revOrigBase, file.revOrigPath)] = relPath(file.base, file.path);
-
-      // Assume renamed files don't need any replacing and pass them through
-      // (we don't want to cache anything huge)
-      this.push(file);
-      cb();
-    } else {
-      cache.push(file);
-      cb();
     }
+
+    if (replaceInExtensions.indexOf(path.extname(file.path)) > -1) {
+      // Cache file to perform replacements in it later.
+      cache.push(file);
+    } else {
+      this.push(file);
+    }
+
+    cb();
   }, function(cb) {
-    // Once we have a full list of renames, search/replace in the non-renamed
+    // Once we have a full list of renames, search/replace in the cached
     // files and push them through.
     var file;
-    for (var i=0, ii=cache.length; i!=ii; i++) {
+    var contents;
+    for (var i = 0, ii = cache.length; i !== ii; i++) {
       file = cache[i];
-      for (path in renames) {
-        if (renames.hasOwnProperty(path)) {
-          file.contents = new Buffer(file.contents.toString().replace(path, renames[path]));
+      contents = file.contents.toString();
+      for (var rename in renames) {
+        if (renames.hasOwnProperty(rename)) {
+          contents = contents.replace(rename, renames[rename]);
         }
       }
+      file.contents = new Buffer(contents);
       this.push(file);
     }
 
